141258181eac64bf8db9695e2bbc8969889de388
Toan3333 <toan21420@gmail.com>
2021-11-23 18:49:00 +0700

    uploade source code

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+var swiper = new Swiper(".testimonial-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+var swiper = new Swiper(".brand-slider", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".our-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  spaceBetween: 30,
+});
+
+const toggle = document.querySelector(".header-bar");
+const menu = document.querySelector(".header-navbar-menu");
+const activeClass = "is-show";
+
+toggle.addEventListener("click", function () {
+  menu.classList.toggle(activeClass);
+});
